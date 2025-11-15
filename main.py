71b2ee345d0982ebf5c2f4f4ad20c5ff4a267aa7
Hologram <cngolden3@gmail.com>
2025-11-15 10:31:54 -0800

    Added CPU throttling

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -14,6 +16,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = (clock.tick(60)/1000)
         pygame.display.flip()
 
 
