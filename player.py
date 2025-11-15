fe73a8fffcf9520b835d537bf9b55aa4a71990e7
Hologram <cngolden3@gmail.com>
2025-11-15 10:36:55 -0800

    Added circleshape.py and player.py

---
+++ player.py
@@ -0,0 +1,12 @@
+import pygame
+import circleshape
+from constants import PLAYER_RADIUS
+
+class Player(CircleShape):
+    def triangle(self):
+    forward = pygame.Vector2(0, 1).rotate(self.rotation)
+    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+    a = self.position + forward * self.radius
+    b = self.position - forward * self.radius - right
+    c = self.position - forward * self.radius + right
+    return [a, b, c]
